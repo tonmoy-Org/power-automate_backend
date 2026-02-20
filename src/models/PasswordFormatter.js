50f51de68fb79d6497540c36e490493ddb25f34d
tonmoyOrg <tonmoy15-5200@diu.edu.bd>
2026-02-20 17:44:57 +0600

    update

--- src/models/PasswordFormatter.js
+++ src/models/PasswordFormatter.js
@@ -25,6 +25,23 @@ const passwordFormatterSchema = new mongoose.Schema({
     timestamps: true
 });
 
+// Index for search functionality
+passwordFormatterSchema.index({ start_add: 'text', end_add: 'text' });
+
+// Virtual for formatter description
+passwordFormatterSchema.virtual('description').get(function() {
+    return `${this.start_add} (${this.start_index}) → ${this.end_add} (${this.end_index})`;
+});
+
+// Static method to check if formatter is in use
+passwordFormatterSchema.statics.isInUse = async function(formatterId) {
+    const PhoneNumber = mongoose.model('PhoneNumber');
+    const count = await PhoneNumber.countDocuments({
+        'password_formatters.id': formatterId.toString()
+    });
+    return count > 0;
+};
+
 const PasswordFormatter = mongoose.model('PasswordFormatter', passwordFormatterSchema);
 
 module.exports = PasswordFormatter;
