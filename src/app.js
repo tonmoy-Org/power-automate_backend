1b4f23a62374073afa3fcf7fd8087e3b67b1283e
tonmoyOrg <tonmoy15-5200@diu.edu.bd>
2026-02-21 13:00:29 +0600

    update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const phoneNumberRoutes = require('./routes/phoneNumberRoutes');
 const passwordFormatterRoutes = require('./routes/passwordFormatterRoutes');
-const phoneCredential  = require('./routes/phoneCredentialRoutes');
+const phoneCredentialRoutes = require('./routes/phoneCredentialRoutes');
 const setupSwagger = require('./config/swagger');
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://power-automate-fontend.vercel.app',
+];
+
 app.use(
   cors({
-    origin: 'https://power-automate-fontend.vercel.app' ,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
@@ -19,26 +31,24 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// after app.use(express.json()) ...
 setupSwagger(app);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/phone-numbers', phoneNumberRoutes);
 app.use('/api/password-formatters', passwordFormatterRoutes);
-app.use('/api/phone-credentials', phoneCredential);
-
+app.use('/api/phone-credentials', phoneCredentialRoutes);
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({ success: true, message: 'Server is running' });
 });
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res
-    .status(500)
-    .json({ success: false, message: 'Internal server error' });
+  res.status(500).json({
+    success: false,
+    message: err.message || 'Internal server error',
+  });
 });
 
 module.exports = app;
